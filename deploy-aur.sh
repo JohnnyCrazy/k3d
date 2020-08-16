eee0710eb59bd2a2f37e08c6266f7ddaab270ed2
Jonas Dellinger <jonas@dellinger.dev>
2020-08-16 16:51:29 +0200

    Update .drone.yml

--- deploy-aur.sh
+++ deploy-aur.sh
@@ -9,18 +9,21 @@ useradd -ms /bin/bash aur
 su aur
 
 # Configuration
-export PACKAGE_NAME=$PLUGIN_PACKAGE_NAME
+export PACKAGE_NAME="$PLUGIN_PACKAGE_NAME"
 export REPO_URL="ssh://aur@aur.archlinux.org/$PACKAGE_NAME.git"
-export NEW_RELEASE=${DRONE_COMMIT_REF##*/v}
-export COMMIT_USERNAME=$PLUGIN_COMMIT_USERNAME
-export COMMIT_EMAIL=$PLUGIN_COMMIT_EMAIL
+export NEW_RELEASE="${DRONE_COMMIT_REF##*/v}"
+export COMMIT_USERNAME="$PLUGIN_COMMIT_USERNAME"
+export COMMIT_EMAIL="$PLUGIN_COMMIT_EMAIL"
+export COMMIT_MESSAGE="$PLUGIN_COMMIT_MESSAGE"
+echo "$COMMIT_MESSAGE"
+exit 0
 echo "---------------- AUR Package version $PACKAGE_NAME/$NEW_RELEASE ----------------"
 
 # SSH & GIT Setup
-mkdir $HOME/.ssh && chmod 700 $HOME/.ssh
-ssh-keyscan -t ed25519 aur.archlinux.org >> $HOME/.ssh/known_hosts
-echo -e "$PLUGIN_SSH_PRIVATE_KEY" | base64 -d > $HOME/.ssh/id_rsa
-chmod 600 $HOME/.ssh/id_rsa
+mkdir "$HOME/.ssh" && chmod 700 "$HOME/.ssh"
+ssh-keyscan -t ed25519 aur.archlinux.org >> "$HOME/.ssh/known_hosts"
+echo -e "$PLUGIN_SSH_PRIVATE_KEY" | base64 -d > "$HOME/.ssh/id_rsa"
+chmod 600 "$HOME/.ssh/id_rsa"
 git config --global user.name "$COMMIT_USERNAME"
 git config --global user.email "$COMMIT_EMAIL"
 
@@ -38,6 +41,6 @@ cat PKGBUILD.template | envsubst '$NEW_RELEASE$SHA256_SUMS_x86_64$SHA256_SUMS_aa
 makepkg --printsrcinfo > .SRCINFO
 echo "------------- BUILD DONE ----------------"
 git add PKGBUILD .SRCINFO
-git commit -m "[ci] Updated to $NEW_RELEASE"
+git commit -m "$COMMIT_MESSAGE"
 git push
 echo "------------- PUBLISH DONE ----------------"
